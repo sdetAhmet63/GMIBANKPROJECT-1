6c2511efe7a0889e993e09cb050007c6c1ceb1c3
hsnbsry <basriakbas@hotmail.com>
2020-12-08 19:23:50 +0100

    basri runner

--- src/test/java/runners/Us_09_10_11Runner.java
+++ src/test/java/runners/Us_09_10_11Runner.java
@@ -1,2 +1,19 @@
-package runners;public class Us_09_10_11Runner {
+package runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict = true,
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
+        features = "src/test/resources/features",
+        glue = "src/test/java/stepdefinitions",
+        tags = "@employee",
+        dryRun = true
+
+
+)
+public class Us_09_10_11Runner {
 }
